fixed attitude calculation when there are multiple spacecrafts

simulator/dynamics/dynamics.py
@@ -39,10 +39,12 @@ class Dynamics:
         
         # Handle ATTITUDE
         
-        attitude = state[state_indices["ATTITUDE"]].reshape(N, 4)
-        angular_velocity = state[state_indices["ANGULAR_VELOCITY"]].reshape(N, 3)
+        K = state_props["SPACECRAFT_INDICES"].shape[0]
         
-        Omega = np.zeros((N, 4, 4))
+        attitude = (state[state_indices["ATTITUDE"]].reshape(N, 4))[state_props["SPACECRAFT_INDICES"]]
+        angular_velocity = (state[state_indices["ANGULAR_VELOCITY"]].reshape(N, 3))[state_props["SPACECRAFT_INDICES"]]
+        
+        Omega = np.zeros((K, 4, 4))
         Omega[:, 1, 0] = angular_velocity[:, 0]
         Omega[:, 2, 0] = angular_velocity[:, 1]
         Omega[:, 3, 0] = angular_velocity[:, 2]
@@ -59,7 +61,8 @@ class Dynamics:
         Omega[:, 1, 3] = - angular_velocity[:, 1]
         Omega[:, 2, 3] = angular_velocity[:, 0]
         
-        d_attitude = .5 * np.einsum('nij,nj->ni', Omega, attitude)
+        d_attitude = np.zeros((N, 4))
+        d_attitude[state_props["SPACECRAFT_INDICES"]] = .5 * np.einsum('nij,nj->ni', Omega, attitude)
         
         d_state[state_indices["ATTITUDE"]] = d_attitude.flatten()
         
@@ -70,9 +73,10 @@ class Dynamics:
         
         Iw = np.einsum('nij,nj->ni', I, angular_velocity)
         cross = np.cross(angular_velocity, Iw, axis=-1)   
-        torque = sum([screw[1] for screw in screws])
+        torque = sum([screw[1] for screw in screws])[state_props["SPACECRAFT_INDICES"]]
         
-        d_angular_velocity = np.einsum('nij,nj->ni', I_inv, torque - cross)
+        d_angular_velocity = np.zeros((N, 3))
+        d_angular_velocity[state_props["SPACECRAFT_INDICES"]] = np.einsum('nij,nj->ni', I_inv, torque - cross)
         
         d_state[state_indices["ANGULAR_VELOCITY"]] = d_angular_velocity.flatten()
 

simulator/environment/environment.py
@@ -32,13 +32,19 @@ class Environment:
         self.state_props: dict[str, np.ndarray] = {
             "MASS": np.zeros(0),
             "INERTIA_MATRIX": np.zeros((0, 3, 3)),
-            "INERTIA_MATRIX_INV": np.zeros((0, 3, 3))
+            "INERTIA_MATRIX_INV": np.zeros((0, 3, 3)),
+            "SPACECRAFT_INDICES": np.zeros(0, dtype=int),
         }
 
     def add_object(self, obj: Object):
         self.objects.append(obj)
+        obj.environment = self
+        ind = len(self.objects) - 1
+        obj.index = ind
+        
         if isinstance(obj, Spacecraft):
             self.spacecrafts.append(obj)
+            self.state_props["SPACECRAFT_INDICES"] = np.append(self.state_props["SPACECRAFT_INDICES"], ind)
             
         new_state_value_count = 0
             
@@ -55,9 +61,6 @@ class Environment:
             self.state_props["INERTIA_MATRIX"] = np.concatenate((self.state_props["INERTIA_MATRIX"], obj.inertia_matrix.reshape(1, 3, 3)), axis=0)
             self.state_props["INERTIA_MATRIX_INV"] = np.concatenate((self.state_props["INERTIA_MATRIX_INV"], np.linalg.inv(obj.inertia_matrix).reshape(1, 3, 3)), axis=0)
 
-        obj.environment = self
-        obj.index = len(self.objects) - 1
-        
         self.state = np.zeros(self.state.size + new_state_value_count)
         
         i = 0